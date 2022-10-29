8ad6ebfaf383d8985a21e398a32a2bf382869953
Trung Pham <phamanhtrung@gmail.com>
2022-10-29 16:50:55 -0500

    add search feature

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Quote from './Quote';
 
 function App() {
   const [data, setData] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
   const getData = () => {
     fetch('quotes_data.json', {headers: {'Content-Type': "application/json", 'Accept': 'application/json'}}).then(res => {
       return res.json()
@@ -20,11 +22,19 @@ function App() {
     <div className="App">
       <div className='Header'>
         Elon Musk
+        <input onChange={event=>setSearchInput(event.target.value)} type="text" placeholder="Search ..."/>
       </div>
 
       <div className='all-quotes'>
         {
-          data && data.length > 0 && data.map((item,i) => <Quote text={item.quote} url={item.url} key={i}/>) 
+          data && data.length > 0 
+          && data.filter(item => {
+            if (searchInput.length == 0) {
+              return item;
+            } else if (item.quote.toLowerCase().includes(searchInput.toLowerCase())) {
+              return item;
+            }
+          }).map((item,i) => <Quote text={item.quote} url={item.url} key={i}/>) 
         }
       </div>
     </div>
